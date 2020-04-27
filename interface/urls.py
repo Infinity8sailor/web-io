7ac69ed2c35e87b2ec1933257528461f023cde72
infinity_sailor <vaibhav.dasharathe18@vit.edu>
2020-04-27 19:43:53 +0530

    static update

--- interface/urls.py
+++ interface/urls.py
@@ -2,10 +2,11 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
