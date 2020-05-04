85f655dfb2758659f76cd6d4e7cc74e74cbbb6cb
infinity_sailor <vaibhav.dasharathe18@vit.edu>
2020-05-04 22:18:19 +0530

    new

--- web_io/settings.py
+++ web_io/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -126,9 +125,5 @@ STATIC_ROOT = "/var/www/web-io/static/"
 
 # List of finder classes that know how to find static files in
 # various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+
 
