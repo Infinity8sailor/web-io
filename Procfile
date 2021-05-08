b0c9e374baf0c82985467eee6b8cb1cf796a7bf8
vaibhav Dasharathe <vinfinitysailor@gmail.com>
2021-05-08 18:16:28 +0530

    ready to deploy heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn web_io.wsgi --log-file -
--- web_io/settings.py
+++ web_io/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3#3!6vx42ln5vv$#6@v$xe1wvc$7s=w91+mt-n$$f023le!*d7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['admin-io.herokuapp.com','127.0.0.1']
 
 
 # Application definition
